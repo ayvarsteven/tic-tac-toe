775fa950a23fe54084a32bdb3485ede99381ba7d
Steven Ayvar <ayvarsteven@gmail.com>
2022-12-16 12:48:14 -0600

    adds function to select interactive html elements

--- app.js
+++ app.js
@@ -1,6 +1,17 @@
-const GameBoard = (() => {
+const ticTacToe = (() => {
   const gameboard = [];
   const player1 = {};
   const player2 = {};
   const game = {};
+
+  const getDomElements = (() => {
+    const mainContainer = document.getElementById('main-container');
+    const turnDisplay = document.getElementById('turn-display');
+    const boardContainer = document.getElementById('board-container');
+    const restartBtn = document.getElementById('restart-btn');
+
+    return { mainContainer, turnDisplay, boardContainer, restartBtn };
+  })();
+
+  return { getDomElements };
 })();
