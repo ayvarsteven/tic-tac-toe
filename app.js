9f065aee68b29171650a3f1ecd2f714ee4326fe7
Steven Ayvar <ayvarsteven@gmail.com>
2022-12-16 09:37:11 -0600

    adds eslint and prettier packages

--- app.js
+++ app.js
@@ -1,16 +1,1 @@
-
-const gameboard = {
-
-}
-
-function sayHello (){
-    
-}
-
-function sayHi( name ){
-    var age=10;
-}
-
-let age=12;
-
-var things = ['cool', "double!!!!"   ]
+const gameboard = {};
